fc206359362a7fbccefed467a3079b8d96467eb0
Chariselyasa123 <ahmad.charis@raharja.info>
2021-12-23 16:31:12 +0700

    update landing page

--- public/js/app.js
+++ public/js/app.js
@@ -25621,16 +25621,14 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm-bundler.js");
 
 
-var _hoisted_1 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<div class=\"flex flex-wrap m-10 mx-12 justify-between\"><div class=\"flex gap-8 items-center\"><a href=\"#\" class=\"uppercase text-2xl font-bold\">Ripala</a><a class=\"font-bold\">Home</a><a class=\"font-bold\">Artikel</a><a class=\"font-bold\">About us</a></div><div class=\"flex gap-8 items-center\"><a href=\"#\" class=\"font-bold\">About us</a><a href=\"#\" class=\"font-bold\">Contact</a></div></div>", 1);
-
-var _hoisted_2 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<div class=\"flex flex-col h-screen\"><div class=\"m-auto flex flex-col items-center\"><h1 class=\"uppercase text-6xl font-bold\">E-Mountaineering</h1><h5 class=\"text-xl uppercase\">Petualangan memberikan kita inspirasi</h5></div><div class=\"m-auto flex flex-col items-center\"><button class=\"text-white bg-gray-800 rounded-full px-8 py-3 flex items-center\"> Aku ingin menjelah <svg xmlns=\"http://www.w3.org/2000/svg\" class=\"h-5 w-5 ml-2 bounce-right\" viewBox=\"0 0 20 20\" fill=\"currentColor\"><path fill-rule=\"evenodd\" d=\"M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z\" clip-rule=\"evenodd\"></path></svg></button></div></div>", 1);
+var _hoisted_1 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<div class=\"bg-[url(&#39;/images/mountain.jpg&#39;)]\"><!-- Landing page header --><div class=\"flex flex-wrap py-10 px-12 justify-between\"><div class=\"flex gap-8 items-center\"><a href=\"#\" class=\"uppercase text-2xl font-bold\">Ripala</a><a class=\"font-bold\">Home</a><a class=\"font-bold\">Artikel</a><a class=\"font-bold\">About us</a></div><div class=\"flex gap-8 items-center\"><a href=\"#\" class=\"font-bold\">About us</a><a href=\"#\" class=\"font-bold\">Contact</a></div></div><!-- Landing page content --><!-- Make content center div --><div class=\"flex flex-col h-screen\"><div class=\"m-auto flex flex-col items-center\"><h1 class=\"uppercase text-6xl font-bold\">E-Mountaineering</h1><h5 class=\"text-xl uppercase\">Petualangan memberikan kita inspirasi</h5></div><div class=\"m-auto flex flex-col items-center\"><button class=\"text-white bg-gray-800 rounded-full px-8 py-3 flex items-center\"> Aku ingin menjelah <svg xmlns=\"http://www.w3.org/2000/svg\" class=\"h-5 w-5 ml-2 bounce-right\" viewBox=\"0 0 20 20\" fill=\"currentColor\"><path fill-rule=\"evenodd\" d=\"M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z\" clip-rule=\"evenodd\"></path></svg></button></div></div></div>", 1);
 
 function render(_ctx, _cache, $props, $setup, $data, $options) {
   var _component_Head = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("Head");
 
   return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_Head, {
     title: "Welcome"
-  }), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)(" Landing page header "), _hoisted_1, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)(" Landing page content "), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)(" Make content center div "), _hoisted_2], 64
+  }), _hoisted_1], 64
   /* STABLE_FRAGMENT */
   );
 }
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,37 +1,38 @@
 <template>
     <Head title="Welcome"/>
+    <div class="bg-[url('/images/mountain.jpg')]">
+        <!-- Landing page header -->
+        <div class="flex flex-wrap py-10 px-12 justify-between">
+            <div class="flex gap-8 items-center">
+                <a href="#" class="uppercase text-2xl font-bold">Ripala</a>
+                <a class="font-bold">Home</a>
+                <a class="font-bold">Artikel</a>
+                <a class="font-bold">About us</a>
+            </div>
+            <div class="flex gap-8 items-center">
+                <a href="#" class="font-bold">About us</a>
+                <a href="#" class="font-bold">Contact</a>
+            </div>
+        </div>
 
-    <!-- Landing page header -->
-    <div class="flex flex-wrap m-10 mx-12 justify-between">
-        <div class="flex gap-8 items-center">
-            <a href="#" class="uppercase text-2xl font-bold">Ripala</a>
-            <a class="font-bold">Home</a>
-            <a class="font-bold">Artikel</a>
-            <a class="font-bold">About us</a>
-        </div>
-        <div class="flex gap-8 items-center">
-            <a href="#" class="font-bold">About us</a>
-            <a href="#" class="font-bold">Contact</a>
-        </div>
-    </div>
-
-    <!-- Landing page content -->
-    <!-- Make content center div -->
-    <div class="flex flex-col h-screen">
-        <div class="m-auto flex flex-col items-center">
-            <h1 class="uppercase text-6xl font-bold">E-Mountaineering</h1>
-            <h5 class="text-xl uppercase">Petualangan memberikan kita inspirasi</h5>
-        </div>
-        <div class="m-auto flex flex-col items-center">
-            <button class="text-white bg-gray-800 rounded-full px-8 py-3 flex items-center">
-                Aku ingin menjelah
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-2 bounce-right" viewBox="0 0 20 20"
-                     fill="currentColor">
-                    <path fill-rule="evenodd"
-                          d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-                          clip-rule="evenodd"/>
-                </svg>
-            </button>
+        <!-- Landing page content -->
+        <!-- Make content center div -->
+        <div class="flex flex-col h-screen">
+            <div class="m-auto flex flex-col items-center">
+                <h1 class="uppercase text-6xl font-bold">E-Mountaineering</h1>
+                <h5 class="text-xl uppercase">Petualangan memberikan kita inspirasi</h5>
+            </div>
+            <div class="m-auto flex flex-col items-center">
+                <button class="text-white bg-gray-800 rounded-full px-8 py-3 flex items-center">
+                    Aku ingin menjelah
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-2 bounce-right" viewBox="0 0 20 20"
+                         fill="currentColor">
+                        <path fill-rule="evenodd"
+                              d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                              clip-rule="evenodd"/>
+                    </svg>
+                </button>
+            </div>
         </div>
     </div>
 </template>
